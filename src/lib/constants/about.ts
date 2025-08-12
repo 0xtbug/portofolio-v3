f3d1401ff24f80505869c0af72c16138fa201aeb
0xtbug <tubagus.nabil19@gmail.com>
2025-08-12 12:22:56 +0700

    fix(about): update resume URL to placeholder to remove specific link

--- src/lib/constants/about.ts
+++ src/lib/constants/about.ts
@@ -1,5 +1,5 @@
 export const ABOUT_INFO = {
   url_linkedin: 'https://www.linkedin.com/in/tubagusnm/',
   url_github: 'https://github.com/0xtbug',
-  url_resume: 'https://drive.google.com/file/d/1c7-UIHy8GUvgj2XHuf7nfhUU4vpjRXSt/view?usp=sharing',
+  url_resume: '#',
 } as const;
